46e233f44508528f9ba896e4b291927087f92c69
William Kent <wjk@sunsol.me>
2016-06-14 16:42:46 -0400

    Set up application

--- AdBlockPlus/ViewController.swift
+++ AdBlockPlus/ViewController.swift
@@ -9,19 +9,15 @@
 import Cocoa
 
 class ViewController: NSViewController {
-
 	override func viewDidLoad() {
 		super.viewDidLoad()
-
+		
 		// Do any additional setup after loading the view.
 	}
-
+	
 	override var representedObject: AnyObject? {
 		didSet {
-		// Update the view, if already loaded.
+			// Update the view, if already loaded.
 		}
 	}
-
-
 }
-

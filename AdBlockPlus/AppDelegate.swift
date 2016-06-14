b3064d6bf637ee38d88ed5865f5b0a444a838916
William Kent <wjk@sunsol.me>
2016-06-14 16:40:08 -0400

    Initial Commit

---
+++ AdBlockPlus/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  AdBlockPlus
+//
+//  Created by William Kent on 6/14/16.
+//  Copyright © 2016 William Kent. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+	func applicationDidFinishLaunching(_ aNotification: Notification) {
+		// Insert code here to initialize your application
+	}
+
+	func applicationWillTerminate(_ aNotification: Notification) {
+		// Insert code here to tear down your application
+	}
+
+
+}
+
---
+++ AdBlockPlus/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  AdBlockPlus
+//
+//  Created by William Kent on 6/14/16.
+//  Copyright © 2016 William Kent. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		// Do any additional setup after loading the view.
+	}
+
+	override var representedObject: AnyObject? {
+		didSet {
+		// Update the view, if already loaded.
+		}
+	}
+
+
+}
+

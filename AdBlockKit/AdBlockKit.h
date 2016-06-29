595c2ad03aa39273113bcc5ee602487185d24451
William Kent <wjk@sunsol.me>
2016-06-29 17:48:22 -0400

    Add AdBlockKit
    This framework will contain code shared between the
    application and the Safari extension.

---
+++ AdBlockKit/AdBlockKit.h
@@ -0,0 +1,12 @@
+//
+//  AdBlockKit.h
+//  AdBlockKit
+//
+//  Created by William Kent on 6/29/16.
+//  Copyright © 2016 William Kent. All rights reserved.
+//
+
+@import Foundation;
+
+extern double AdBlockKitVersionNumber;
+extern const unsigned char AdBlockKitVersionString[];

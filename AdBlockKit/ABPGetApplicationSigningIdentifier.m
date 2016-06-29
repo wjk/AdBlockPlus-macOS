79edc2713e3720979f29845bb5345c9e59bfe2d7
William Kent <wjk@sunsol.me>
2016-06-29 19:47:21 -0400

    Move Team ID code inside of dispatch_once

--- AdBlockKit/ABPGetApplicationSigningIdentifier.m
+++ AdBlockKit/ABPGetApplicationSigningIdentifier.m
@@ -16,17 +16,23 @@ static void SafeCFRelease(CFTypeRef cf) {
 #define AutoCFRelease __attribute__((cleanup(SafeCFRelease)))
 
 NSString *ABPGetApplicationSigningIdentifier(void) {
-	AutoCFRelease SecCodeRef processCodeObject = NULL;
-	OSStatus status = SecCodeCopySelf(kSecCSDefaultFlags, &processCodeObject);
-	NSCAssert(status == noErr, @"SecCodeCopySelf returned status %d", status);
-	NSCAssert(processCodeObject, @"SecCodeCopySelf returned success, but gave NULL SecCodeRef");
+	static NSString *teamID = NULL;
+	static dispatch_once_t onceToken;
+	dispatch_once(&onceToken, ^{
+		AutoCFRelease SecCodeRef processCodeObject = NULL;
+		OSStatus status = SecCodeCopySelf(kSecCSDefaultFlags, &processCodeObject);
+		NSCAssert(status == noErr, @"SecCodeCopySelf returned status %d", status);
+		NSCAssert(processCodeObject, @"SecCodeCopySelf returned success, but gave NULL SecCodeRef");
 
-	AutoCFRelease CFDictionaryRef codeAttributes = NULL;
-	status = SecCodeCopySigningInformation(processCodeObject, kSecCSSigningInformation, &codeAttributes);
-	NSCAssert(status == noErr, @"SecCodeCopySigningInformation returned status %d", status);
-	NSCAssert(codeAttributes != NULL, @"SecCodeCopySigningInformation returned success, but gave NULL attributes dictionary");
+		AutoCFRelease CFDictionaryRef codeAttributes = NULL;
+		status = SecCodeCopySigningInformation(processCodeObject, kSecCSSigningInformation, &codeAttributes);
+		NSCAssert(status == noErr, @"SecCodeCopySigningInformation returned status %d", status);
+		NSCAssert(codeAttributes != NULL, @"SecCodeCopySigningInformation returned success, but gave NULL attributes dictionary");
 
-	CFStringRef teamID = CFDictionaryGetValue(codeAttributes, kSecCodeInfoTeamIdentifier);
-	NSCAssert(teamID != NULL, @"Could not retrieve Team Identifier");
-	return (__bridge_transfer NSString *)teamID;
+		CFStringRef teamIDCF = CFDictionaryGetValue(codeAttributes, kSecCodeInfoTeamIdentifier);
+		NSCAssert(teamIDCF != NULL, @"Could not retrieve Team Identifier");
+		teamID = (__bridge_transfer NSString *)teamIDCF;
+	});
+
+	return teamID;
 }

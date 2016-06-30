5c9a01343dee1b43d90d9f822915d997ebfe4938
William Kent <wjk@sunsol.me>
2016-06-30 14:44:45 -0400

    Add ButtonTableCellView

---
+++ AdBlockPlus/UtilityViews.swift
@@ -0,0 +1,14 @@
+//
+//  UtilityViews.swift
+//  AdBlockPlus
+//
+//  Created by William Kent on 6/30/16.
+//  Copyright © 2016 William Kent. All rights reserved.
+//
+
+import Foundation
+import AppKit
+
+class ButtonTableCellView: NSTableCellView {
+	@IBOutlet internal var button: NSButton!
+}

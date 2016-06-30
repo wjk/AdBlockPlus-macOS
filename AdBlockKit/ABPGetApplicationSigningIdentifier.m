a678f52b0045a8d56e232f861c914b03c457c97e
William Kent <wjk@sunsol.me>
2016-06-30 13:54:50 -0400

    Fix crashing bug

--- AdBlockKit/ABPGetApplicationSigningIdentifier.m
+++ AdBlockKit/ABPGetApplicationSigningIdentifier.m
@@ -9,22 +9,26 @@
 @import Foundation;
 @import Security;
 
-static void SafeCFRelease(CFTypeRef cf) {
-	if (cf != NULL) CFRelease(cf);
+static void SafeCFRelease_CFDictionaryRef(CFDictionaryRef *cf) {
+	if (*cf != NULL) CFRelease(*cf);
 }
 
-#define AutoCFRelease __attribute__((cleanup(SafeCFRelease)))
+static void SafeCFRelease_SecCodeRef(SecCodeRef *cf) {
+	if (*cf != NULL) CFRelease(*cf);
+}
+
+#define AutoCFReleased(type) __attribute__((cleanup(SafeCFRelease_##type))) type
 
 NSString *ABPGetApplicationSigningIdentifier(void) {
 	static NSString *teamID = NULL;
 	static dispatch_once_t onceToken;
 	dispatch_once(&onceToken, ^{
-		AutoCFRelease SecCodeRef processCodeObject = NULL;
+		AutoCFReleased(SecCodeRef) processCodeObject = NULL;
 		OSStatus status = SecCodeCopySelf(kSecCSDefaultFlags, &processCodeObject);
 		NSCAssert(status == noErr, @"SecCodeCopySelf returned status %d", status);
 		NSCAssert(processCodeObject, @"SecCodeCopySelf returned success, but gave NULL SecCodeRef");
 
-		AutoCFRelease CFDictionaryRef codeAttributes = NULL;
+		AutoCFReleased(CFDictionaryRef) codeAttributes = NULL;
 		status = SecCodeCopySigningInformation(processCodeObject, kSecCSSigningInformation, &codeAttributes);
 		NSCAssert(status == noErr, @"SecCodeCopySigningInformation returned status %d", status);
 		NSCAssert(codeAttributes != NULL, @"SecCodeCopySigningInformation returned success, but gave NULL attributes dictionary");
